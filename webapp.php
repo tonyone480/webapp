1f7bd8e62815c67cd2f20c62976679d2be435880
zeroneta <zeroneta@qq.com>
2021-11-01 19:05:54 +0800

    main

--- webapp.php
+++ webapp.php
@@ -335,7 +335,7 @@ abstract class webapp implements ArrayAccess, Stringable
 		return inet_ntop(hex2bin($hex));
 	}
 	//---------------------
-	function src($filename):string
+	function rootsrc($filename):string
 	{
 		return "{$this['app_sources']}{$filename}";
 	}
