d2548292ff924fa2a5f6b2aa775366059debca5f
codedk47 <zeroneta47@icloud.com>
2022-05-25 11:44:06 +0700

    1

--- app/news/driver.js
+++ app/news/driver.js
@@ -1,11 +1,11 @@
-function request(method, url, send = null)
+function request(method, url, data = null)
 {
     return new Promise(function(ok, no){
         const xhr = new XMLHttpRequest;
         xhr.open(method, url);
         xhr.onload = ok;
         xhr.onerror = no;
-        xhr.send(send);
+        xhr.send(data);
     });
 }
 
